3f82add75600d3f5fec5895560afba20fff3ebc2
Arnold Graaff <arnoldgr@gmail.com>
2017-08-09 14:57:41 +0200

    Refactored boilerplate code to Thymeleaf fragments

--- src/main/java/com/devopsbuddy/DevopsbuddyApplication.java
+++ src/main/java/com/devopsbuddy/DevopsbuddyApplication.java
@@ -6,7 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DevopsbuddyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DevopsbuddyApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(DevopsbuddyApplication.class, args);
+
+        String name = "X";
+
+        for (int x = 0; x < 56; x++) {
+            System.out.println(name);
+        }
+
+    }
 }
--- src/main/java/com/devopsbuddy/web/controllers/IndexController.java
+++ src/main/java/com/devopsbuddy/web/controllers/IndexController.java
@@ -1,7 +1,15 @@
 package com.devopsbuddy.web.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by Arnold on 2017/08/07.
  */
+@Controller
 public class IndexController {
+    @RequestMapping("/")
+    public String home(){
+        return "index";
+    }
 }
